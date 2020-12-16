eb47ad23cbd78f3f9e2377c3ac3b6889cae56634
Matt Raymond <m4tt.r4ymond@gmail.com>
2020-12-15 23:24:41 -0500

    fixed bugs and comments

--- matt_code/continuous_server.c
+++ matt_code/continuous_server.c
@@ -1,4 +1,11 @@
-// UDP Server: starting earlier + ending later; waiting for request and then sending the response
+/*
+ * Matt Raymond, December 2020
+ * cFS Receiving Socket Library Example
+ * 
+ * EECS 587, Advanced Operating Systems
+ * Final Project
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -12,12 +19,16 @@
 
 #include "receiving_socket.h"
 
+// This is an example application of how to use the cFS receiving socket library
 int main()
 {
+    // allocate buffer, sockets, and incoming data. These will be controlled by the user beause they will likely
+    // want to access them at some point.
     char buffer[MAX_BURST_LEN];
     Sockets socket_data;
     Incoming_Data incoming_data;
 
+    // Initialize the socket, just like any other application
     if (initialize_socket(&socket_data, &incoming_data) != 0)
     {
         printf("There was an issue initializing the socket\n");
@@ -25,19 +36,21 @@ int main()
     }
 
     while (1)
-    {
-        int val = socket_run(&socket_data, buffer, &incoming_data);
-
-        if(val)
+    {   
+        // The socket is considered to fail every time it doesn't find all fo the packets, therefore returning 1
+        // every single time except for the times that it receives packets
+        if (socket_run(&socket_data, buffer, &incoming_data) != 1)
         {
+            // Print out the data just becase
             printf("State_len: %d, Table_len: %d, App_len: %d, Msg_len: %d\n", incoming_data.state.len, incoming_data.sch.len, incoming_data.app.len, incoming_data.msg.len);
-            printf("Test State: %x, %x, %x, %x\n", incoming_data.state.timestamp, incoming_data.state.app_id, incoming_data.state.pointer_offset, incoming_data.state.len);
+            // Free all of the mallocs and reset all of the variables so we don't end up with memory leaks
             destroy(&incoming_data);
         }
 
         sleep(1);
     }
 
+    // get ready to shut down
     uninitialize_socket(&socket_data);
     return EXIT_SUCCESS;
 }
--- matt_code/receiving_socket.h
+++ matt_code/receiving_socket.h
@@ -1,4 +1,11 @@
-// UDP Server: starting earlier + ending later; waiting for request and then sending the response
+/*
+ * Matt Raymond, December 2020
+ * cFS Receiving Socket Library
+ * 
+ * EECS 587, Advanced Operating Systems
+ * Final Project
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -10,60 +17,77 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
+// The UDP port to listen on
 #define INET_UDP_SERVER_PORT 8082
 // Max UDP datagram size
 #define SERVER_BUFFER_MAX 65535
-#define MAX_BURST_LEN SERVER_BUFFER_MAX * 3
+// 4 times the max UDP size, allowing for four packets
+#define MAX_BURST_LEN SERVER_BUFFER_MAX * 4
 
-// Need to increase these all by one to prevent cycling on 0
+// ID for the state packet
 #define ID_STATE 1
+// ID for the schedule packet (schedule table)
 #define ID_SCH 2
+// ID for the app packet (.so file)
 #define ID_APP 3
+// ID for the message packet (message table)
 #define ID_MSG 4
 
-const int OFFSET_PT = 0;
-const int OFFSET_TIME = OFFSET_PT + sizeof(unsigned char);
-const int OFFSET_ID = OFFSET_TIME + sizeof(int32_t);
-const int OFFSET_PNTR = OFFSET_ID + sizeof(int16_t);
-const int OFFSET_LEN = OFFSET_PNTR + sizeof(int16_t);
-const int OFFSET_MESSAGE = OFFSET_LEN + sizeof(int32_t);
+// The initial offset for the packet type id
+#define OFFSET_PT 0
+// The offset for the timestamp
+#define OFFSET_TIME OFFSET_PT + sizeof(unsigned char)
+// The offset for the ID
+#define OFFSET_ID OFFSET_TIME + sizeof(int32_t)
+// The offset for the begining of the message
+#define OFFSET_MESSAGE OFFSET_ID + sizeof(int32_t)
 
-// Fix to return 0 for successful
-// add another table option (message table, schedule table)
-
+// A struct to act as a pseudo struct
 typedef struct Data_Len_Pair
 {
+    // The data to be held, in bytes
     char *data;
+    // the length of the data being held
     int32_t len;
 } Data_Len_Pair;
 
+// A struct that holds the state info that we want to pass on to the developer
 typedef struct State_Info
 {
-    // unsigned char packet_type;
+    // The time that the state was sent
     int32_t timestamp;
-    int16_t app_id;
-    int16_t pointer_offset;
+    // The state being sent, in bytes
     char *data;
+    // the length of the data being held
     int32_t len;
 } State_Info;
 
+// All od the data that's being sent. There is one variable per field
 typedef struct Incoming_Data
 {
+    // The application state that has been sent
     State_Info state;
+    // the schedule table
     Data_Len_Pair sch;
+    // the application .so
     Data_Len_Pair app;
+    // the message table
     Data_Len_Pair msg;
 } Incoming_Data;
 
+// Holds socket data so that developers don't have to worry about it
 typedef struct Sockets
 {
-    int socket_fd; // socket file descriptor
+    // socket file descriptor
+    int socket_fd;
+    // server ip address
     struct sockaddr_in srv_addr;
+    // client ip address
     struct sockaddr_in clnt_addr;
 } Sockets;
 
 int socket_run(Sockets *socket_data, char *buffer, Incoming_Data *incoming_data);
-void process_tbl_app(char *datagram, Data_Len_Pair *incoming_data, int32_t data_len, int type);
+void process_tbl_app(char *datagram, Data_Len_Pair *incoming_data, int32_t data_len);
 void process_state(char *datagram, int data_len, State_Info *info);
 int is_full(Incoming_Data *incoming_data);
 void destroy(Incoming_Data *incoming_data);
@@ -72,129 +96,187 @@ void uninitialize_socket(Sockets *socket_data);
 int initialize_socket(Sockets *socket_data, Incoming_Data *incoming_data);
 int check_buffer(int *socket_fd, char *buffer, struct sockaddr_in *clnt_addr);
 
+// Performs a single socketcheck and data grab (if there's any data in the buffer)
 int socket_run(Sockets *socket_data, char *buffer, Incoming_Data *incoming_data)
 {
+    // declare variables to hold the return value and the buffer size for later
     int ret_val = 0;
-    int buffer_size;
+    int buffer_size = 0;
+
+    // This do loop allows the library to grab packets one at a time if they arive far apart,
+    // but also allows them to be loaded quickly if there are multiple in the buffer at the same time.
+    // This helps keep the buffer from overflowing
     do
     {
+        // If there was anything in the buffer (only happens when looping)
         if (buffer_size > 0)
         {
+            // Sleep for a short amount of time
             usleep(10);
         }
+
+        // Check to see how much data is in the buffer
         buffer_size = check_buffer(&socket_data->socket_fd, buffer, &socket_data->clnt_addr);
 
+        // If there's anything in the buffer
         if (buffer_size > 0)
         {
+            // Process one packet and save the value to ret_val
+            // 0 = success (all were loaded)
+            // 1 = failure (not all were loaded)
             ret_val = process_incoming(buffer, buffer_size, incoming_data);
         }
-    } while (buffer_size > 0 && ret_val);
 
-    return ret_val;
+        // if there is something in the buffer and if it hasn't gotten all of the aailable packets
+        // WARN: This does not get rid of excess packets and will not handle dropped packets
+        // However, Andrew said to assume that we should operate under the assumtion that no packets
+        // can be dropped
+    } while (buffer_size > 0 && !ret_val);
+
+    // Return the result
+    return !ret_val;
 }
 
-void process_tbl_app(char *datagram, Data_Len_Pair *incoming_data, int32_t data_len, int type)
+
+// Loads the message table, schedule table, and app .so
+void process_tbl_app(char *datagram, Data_Len_Pair *incoming_data, int32_t data_len)
 {
+    // Ignores small, malformed packets
+    if (data_len <= 1)
+        return; 
+
+    // If there is any data, reset free the old data
     if (incoming_data->len > 0)
         free(incoming_data->data);
 
-    incoming_data->len = ntohs(*((uint16_t *)(datagram + 1)));
+    // Calculate the appropriate length
+    incoming_data->len = data_len - 1;//ntohs(*((uint16_t *)(datagram + 1)));
+    // malloc space for the data
     incoming_data->data = (char *)malloc(incoming_data->len);
-    memcpy(incoming_data->data, datagram + 3, incoming_data->len);
+    // memcpy data from the buffer to the new malloced array
+    memcpy(incoming_data->data, datagram + 1, incoming_data->len);
 }
 
+
+// Loads the state from a datagram
 void process_state(char *datagram, int data_len, State_Info *info)
 {
+    // Ignores small, malformed packets
+    if (data_len <= 1)
+        return;
+
+    // If there is any data, free it
     if (info->len > 0)
         free(info->data);
-
-    info->timestamp = ntohl(*((uint32_t *)(datagram + OFFSET_TIME)));
-    info->app_id = ntohs(*((uint16_t *)(datagram + OFFSET_ID)));
-    info->pointer_offset = ntohs(*((uint16_t *)(datagram + OFFSET_PNTR)));
-    info->len = ntohl(*((uint32_t *)(datagram + OFFSET_LEN)));
-
-    // Need to switch the byte order? I don't think so, because the message seemed to come through fine
+    
+    // Gets the timestamp from the data
+    info->timestamp = ntohl(*((uint32_t *)(datagram + OFFSET_TIME )));
+    // Calculates the length of the data
+    info->len = data_len - OFFSET_MESSAGE;
+    // mallocs soace for the data
     info->data = (char *)malloc(info->len);
-
+    // copy's data from the buffer to the malloced array
     memcpy(info->data, datagram + OFFSET_MESSAGE, info->len);
 }
 
+
+// Checks to see if all of the data is loaded or not
 int is_full(Incoming_Data *incoming_data)
 {
-    return incoming_data->state.len && incoming_data->sch.len && incoming_data->app.len && incoming_data->msg.len;
+    // Checks to see if all of the lengths are greater than 0
+    return incoming_data->state.len > 0 && incoming_data->sch.len > 0 && incoming_data->app.len > 0 && incoming_data->msg.len > 0;
 }
 
+
+// Frees all of the data and sets values back to default
 void destroy(Incoming_Data *incoming_data)
 {
+    // Free everything
     free(incoming_data->state.data);
     free(incoming_data->sch.data);
     free(incoming_data->app.data);
     free(incoming_data->msg.data);
+
+    // Reset lengths to 0 to show that there's nothing in there
     incoming_data->state.len = incoming_data->sch.len = incoming_data->msg.len = incoming_data->app.len = 0;
+
+    // Setting pointers to NULL so that free does't throw an error if accidentaly called again
+    incoming_data->state.data = incoming_data->sch.data = incoming_data->msg.data = incoming_data->app.data = NULL;
 }
 
+
+// Determines which type of message a packet is and retreives the data accordingly
 int process_incoming(char *buffer, int buffer_size, Incoming_Data *incoming_data)
 {
-    if (buffer_size < 1)
-        return 0;
-
+    // Packet is for the state
     if (buffer[OFFSET_PT] == ID_STATE)
     {
-        printf("state\n");
         process_state(buffer, buffer_size, &incoming_data->state);
     }
+    // Packet is for the app .so
     else if (buffer[OFFSET_PT] == ID_APP)
     {
-        printf("app\n");
-        process_tbl_app(buffer, &incoming_data->app, buffer_size, buffer[OFFSET_PT]);
+        process_tbl_app(buffer, &incoming_data->app, buffer_size);
     }
+    // Packet is for the schedule table
     else if (buffer[OFFSET_PT] == ID_SCH)
     {
-        printf("schedule\n");
-        process_tbl_app(buffer, &incoming_data->sch, buffer_size, buffer[OFFSET_PT]);
+        process_tbl_app(buffer, &incoming_data->sch, buffer_size);
     }
+    // Packet is for the message table
     else if (buffer[OFFSET_PT] == ID_MSG)
     {
-        printf("message\n");
-        process_tbl_app(buffer, &incoming_data->msg, buffer_size, buffer[OFFSET_PT]);
+        process_tbl_app(buffer, &incoming_data->msg, buffer_size);
     }
 
-    return !is_full(incoming_data);
+    // If it was full, then the method was successful and returns a bool
+    return is_full(incoming_data);
 }
 
+
+// Close the socket
 void uninitialize_socket(Sockets *socket_data)
 {
     close(socket_data->socket_fd);
 }
 
+
+// Set up the socket and some basic data for receiving
 int initialize_socket(Sockets *socket_data, Incoming_Data *incoming_data)
 {
+    // Set to 0 to initialize
     incoming_data->state.len = incoming_data->sch.len = incoming_data->msg.len = incoming_data->app.len = 0;
-
     memset(&socket_data->srv_addr, 0, sizeof(socket_data->srv_addr));
     memset(&socket_data->clnt_addr, 0, sizeof(socket_data->clnt_addr));
 
+    // initializes the socket
     if ((socket_data->socket_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
     {
         perror("Socket Creation Failure");
         return EXIT_FAILURE;
     }
 
+    // Set up the socket data
     (&socket_data->srv_addr)->sin_family = PF_INET;
     (&socket_data->srv_addr)->sin_addr.s_addr = INADDR_ANY;
     (&socket_data->srv_addr)->sin_port = htons(INET_UDP_SERVER_PORT);
 
+    // bind the socket to a part
     if (bind(socket_data->socket_fd, (const struct sockaddr *)(&socket_data->srv_addr), sizeof(socket_data->srv_addr)) < 0)
     {
         perror("Binding Failure");
         return EXIT_FAILURE;
     }
 
+    // Return success
     return 0;
 }
 
+// Check to see if there's anything in the buffer
 int check_buffer(int *socket_fd, char *buffer, struct sockaddr_in *clnt_addr)
 {
+    // variable to hold data that we don't need
     unsigned int clnt_addrSize = sizeof(*clnt_addr);
+    // receive data from the socket and return the length
     return recvfrom(*socket_fd, (char *)buffer, SERVER_BUFFER_MAX, MSG_DONTWAIT, (struct sockaddr *)clnt_addr, &clnt_addrSize);
 }
